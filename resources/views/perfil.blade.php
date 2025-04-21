7a79606a0e76a3090df2a97cea74eda8741b5d5f
devfabk <devfabk@gmail.com>
2025-04-21 20:32:44 +0200

    Estilos pre-backend para el perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -2,37 +2,55 @@
 
 @section('perfil', 'Perfil')
 
-@section('content')
+@section('styles')
+<link href="{{ asset('css/estilos-perfil.css') }}" rel="stylesheet" />
+@endsection
 
-<div class="contenido">
-    <div class="informacion">
-        <div class="personal">
-            <div class="foto"></div>
-            <div class="nombre"></div>
-            <div class="correo"></div>
+@section('content')
+<div class="fondo">
+    <div class="contenido">
+        <div class="informacion">
+            <div class="personal">
+                <div class="foto"></div>
+                <div class="info">
+                    <div class="nombre">NOMBRE</div>
+                    <div class="correo">CORREO</div>
+                </div>
+            </div>
+            <div class="puntos">
+                <h2>PUNTOS DE DEBATE:</h2>
+                <div class="puntos-valor">
+                    <span class="puntos-numero">Nx</span>
+                    <span class="icono">ICO</span>
+                </div>
+            </div>
         </div>
-        <div class="puntos">
-            <h2>PUNTOS DE DEBATE: </h2>
+        <div class="publicaciones">
+            <div class="participado">
+                <h3>HAS PARTICIPADO EN:</h3>
+                <hr>
+                <ul>
+                    <li><a href="#">TEMA 1</a></li>
+                    <li>TEMA 2</li>
+                    <li>TEMA 3</li>
+                    <li>TEMA 3</li>
+                </ul>
+            </div>
+            <div class="mejor-argumento">
+                <h3>TU MEJOR ARGUMENTO</h3>
+                <div class="argumento">
+                    <div class="argumento-titulo">ARGUMENTO CON MAS VOTOS</div>
+                    <div class="argumento-contenido">
+                        <div class="linea-ondulada"></div>
+                        <div class="linea-ondulada"></div>
+                        <div class="linea-ondulada"></div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="publicaciones">
-        <div class="participado">
-            <h3>Has participado en: </h3>
-            <hr>
-            <ul>
-                <li><a href="#">TEMA 1</a></li>
-                <li>TEMA 2</li>
-                <li>TEMA 3</li>
-                <li>TEMA 4</li>
-            </ul>
+        <div class="borrar">
+            <button>BORRAR</button>
         </div>
-        <div class="mejor-argumento">
-            <h3>TU ARGUMENTO MAS VOTADO</h3>
-            <div class="argumento overflow-y-hidden"></div>
-        </div>
-    </div>
-    <div class="borrar">
-        <button>BORRAR PERFIL</button>
     </div>
 </div>
 @endsection
