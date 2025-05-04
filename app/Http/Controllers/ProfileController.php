b2b16059f4dc6562e34fbd5258a92d36e04071e8
devfabk <devfabk@gmail.com>
2025-05-04 19:46:51 +0200

    Creada la vista del perfil de un usuario

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Models\Argumento;
+class ProfileController extends Controller
+{
+    public function mostrarPerfil()
+    {
+        $user = Auth::user();
+
+        $temasParticipados = $user->argumentos()
+        ->with('debate.tema')
+        ->get()
+        ->pluck('debate.tema')
+        ->unique('id');
+
+        $mejorArgumento = Argumento::join('puntuaciones', 'argumentos.id', '=', 'puntuaciones.argumento_id')
+        ->where('argumentos.usuario_id', $user->id)
+        ->orderByDesc('puntuaciones.puntuacion')
+        ->select('argumentos.*') 
+        ->first();
+        
+        return view('perfil', compact('user', 'temasParticipados', 'mejorArgumento'));
+    }
+}
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -7,8 +7,30 @@
 @endsection
 
 @section('content')
-<div class="fondo">
-    <div class="contenido">
+
+    <div class="contenido-perfil">
+        <div class="info-personal">
+            <img src="{{ $user->foto_perfil }}" alt="Foto de perfil" class="foto-perfil">
+            <div class="informacion">
+                <h3>{{ $user->nombre }}</h3>
+                <p>{{ $user->email }}</p>
+            </div>
+            <div class="puntos">
+                <span>Puntos de debate: {{ $user->puntos_de_debate }}</span>
+            </div>
+        </div>
+        <div class="participaciones">
+            <div class="participacion" data-label="Has participado en">
+                <ul class="participacion-list">
+                    @foreach( $temasParticipados as $tema)
+                    <li>{{ $tema->titulo }}</li> <!-- HACER QUE ESTOS SEAN UN LINK, CREAR UN TEMACONTROLLER (NO SIRVE EL BUSCADORTEMACONTROLLER) -->
+                    @endforeach
+                </ul>
+            </div>
+            <div class="mejor-argumento" data-label="Mejor argumento">
+                    {{ $mejorArgumento->contenido }}
+            </div>
+        </div>
     </div>
-</div>
+
 @endsection
