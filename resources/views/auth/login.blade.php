8b38fe1ac921d639b68717d6b424e916b1458d21
devfabk <devfabk@gmail.com>
2025-05-25 21:49:55 +0200

    Añadido el old para el nombre

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,13 +17,13 @@
             <div class="titulo">THE DEBATE CLUB</div>
 
             @if ($errors->any())
-            <div class="errores" style="color: red; font-weight: bold; margin-bottom: 15px;">
-                <ul class="lista-errores">
-                    @foreach ($errors->all() as $error)
-                    <li class="item-lista-errores">{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
+                <div class="errores" style="color: red; font-weight: bold; margin-bottom: 15px;">
+                    <ul class="lista-errores">
+                        @foreach ($errors->all() as $error)
+                            <li class="item-lista-errores">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
 
             <div class="formulario-contenedor">
@@ -32,19 +32,24 @@
 
                     <div class="contenedor__entrada" data-label="Nombre">
                         <div class="icono__contenedor">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round">
                                 <path d="M18 20a6 6 0 0 0-12 0" />
                                 <circle cx="12" cy="10" r="4" />
                                 <circle cx="12" cy="12" r="10" />
                             </svg>
                         </div>
-                        <input type="text" name="nombre" placeholder="Nombre" class="entrada__busqueda" />
+                        <input type="text" name="nombre" placeholder="Nombre" class="entrada__busqueda"
+                            value="{{ old('nombre') }}" />
                         <div class="sombra"></div>
                     </div>
 
                     <div class="contenedor__entrada" data-label="Contraseña">
                         <div class="icono__contenedor">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round">
                                 <circle cx="12" cy="16" r="1" />
                                 <rect x="3" y="10" width="18" height="12" rx="2" />
                                 <path d="M7 10V7a5 5 0 0 1 10 0v3" />
